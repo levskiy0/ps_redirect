e4c901bddef2fb43654b650bdfc0a890a2c6b6ae
DmVsk <dm.vsk@yandex.ru>
2023-09-28 01:52:13 +0700

    Updates avif

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'prestashop/ps_redirect',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'db892830eb2b671bfce4c5c5cb75b7c9be298442',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'prestashop/ps_redirect' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'db892830eb2b671bfce4c5c5cb75b7c9be298442',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
